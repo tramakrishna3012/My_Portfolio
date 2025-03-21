08c1c13aeb74130bfc5caff2a2d6808fea883180
T Rama Krishna <117737866+tramakrishna3012@users.noreply.github.com>
2025-03-21 14:05:31 +0530

    Update ProfessionalExperience.tsx

--- src/components/experience/ProfessionalExperience.tsx
+++ src/components/experience/ProfessionalExperience.tsx
@@ -7,63 +7,55 @@ const ProfessionalExperience = () => {
       <h2>Professional Experience</h2>
       <div className="experience__container container grid grid-cols-1 gap-[2rem] max-sm:gap-[1rem]">
         <div className="experience__item col-span-1 max-md:mx-auto max-md:my-0 max-md:w-[85%] max-md:p-[2rem] max-sm:w-[100%] max-sm:px-[1rem] max-sm:py-[2rem]">
-          <h3 className="color-primary mb-[1rem]">Data Analyst</h3>
-          <h4 className="text-light">Ozibook | Remote, Bangalore, India</h4>
-          <p className="text-light">October 2024 – Present</p>
+          <h3 className="color-primary mb-[1rem]">Data Science Intern</h3>
+          <h4 className="text-light">Personifwy | Remote, Bangalore, India</h4>
+          <p className="text-light">June 2023 – August 2023</p>
           <ul className="list-disc pl-5 mt-2">
             <li>
-              Optimized SQL Queries and Interactive Dashboards: Enhanced SQL
-              query performance by 20%, enabling faster data processing, and
-              developed Power BI dashboards, improving data visualization and
-              decision-making processes by 15%.
+            Credit card Fraud Exploratory Data Analysis: Analysed credit-related data to support risk assessment and fraud prevention, using Python for data cleaning,
+            visualization, and modelling, contributing to reducing financial risks like loan defaults and credit card fraud.
             </li>
             <li>
-              Predictive Analytics Implementation: Built machine learning models
+              Decision-Making Analytics Implementation: Built machine learning models
               using Python libraries such as Pandas, NumPy, Matplotlib, Scrapy,
-              and Scikit-Learn, achieving an 18% increase in predictive
-              accuracy.
+              and Scikit-Learn, achieving an 18% increase in model accuracy.
             </li>
           </ul>
         </div>
 
         <div className="experience__item col-span-1 max-md:mx-auto max-md:my-0 max-md:w-[85%] max-md:p-[2rem] max-sm:w-[100%] max-sm:px-[1rem] max-sm:py-[2rem]">
           <h3 className="color-primary mb-[1rem]">
-            AI Engineer (Machine Learning)
+          J.P. Morgan Software Engineering Virtual Experience
           </h3>
           <h4 className="text-light">
-            Digital Empowerment Networks | Islamabad, Pakistan
+            Forage | Remote, India
           </h4>
-          <p className="text-light">July – September 2024</p>
+          <p className="text-light">August 2024</p>
           <ul className="list-disc pl-5 mt-2">
             <li>
-              Advanced Data Processing: Conducted data cleaning, preprocessing,
-              and visualization to prepare large datasets for analysis,
-              enhancing data quality by 30%.
+              Stock Price Analytic Dashboard: Set up a dev environment, fixed repository issues, and used JPMorgan Chase’s Perspective library to create a live data
+              visualization for traders.
             </li>
             <li>
-              Machine Learning Models: Implemented concept such as k-means
-              clustering, logistic regression, and NLP tokenizing to build
-              predictive models, improving fraud detection accuracy by 22% and
-              reducing false positives by 99.05%.
+              Software Tools: Use Git for version control and managing code changes. JPMorgan Chase's open-source library for data visualization and use other tools to make website frontend Python, Javascript, Typescript, React, HTML, NodeJS, nvm. 
             </li>
           </ul>
         </div>
 
         <div className="experience__item col-span-1 max-md:mx-auto max-md:my-0 max-md:w-[85%] max-md:p-[2rem] max-sm:w-[100%] max-sm:px-[1rem] max-sm:py-[2rem]">
-          <h3 className="color-primary mb-[1rem]">Data Analyst Intern</h3>
-          <h4 className="text-light">Mentorness | Remote, Pakistan</h4>
-          <p className="text-light">May 2024 – June 2024</p>
+          <h3 className="color-primary mb-[1rem]">KPMG AU Data Analytics Consulting Virtual Internship</h3>
+          <h4 className="text-light">Forage | Remote, India</h4>
+          <p className="text-light">May 2024 </p>
           <ul className="list-disc pl-5 mt-2">
             <li>
-              Power BI Dashboards, Presentation Skills: Developed interactive
+              Power BI Dashboards: Completed a KPMG AU Data Analytics simulation, assessing data quality, targeting high-value customers, and developing
+              visual dashboards to communicate findings. 
+            </li>
+            <li>
+              Presentation Skills: Developed interactive
               Power BI dashboards, enhancing data visualization and
               decision-making resulting in a 20% boost in report efficiency and
               15% improvement in decision-making accuracy.
-            </li>
-            <li>
-              SQL Data Management: Optimized SQL queries for data extraction
-              and analysis, enhancing data retrieval speed by 25% and ensuring
-              accurate data handling.
             </li>
           </ul>
         </div>
