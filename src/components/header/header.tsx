a3da8f03ec046701d9e64c6f0d4b5f2c75eaed2d
T Rama Krishna <117737866+tramakrishna3012@users.noreply.github.com>
2025-07-04 03:45:14 +0530

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -32,7 +32,7 @@ const Header = () => {
           </a>
           <div className="flex items-center gap-4">
             <a
-              href="src/components/header/T.Rama_Krishna.pdf"
+              href="/T.Rama_Krishna.pdf"
               download="T.Rama_Krishna_Resume.pdf"
               className="btn-header h-[48px] rounded-full px-6 text-sm font-medium text-white flex items-center justify-center"
             >
